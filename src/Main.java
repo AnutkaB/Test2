133fbd87d77d9c40634284e385cd4e4a4673c295
Anna Bystrova <aabystrovy@mail.ru>
2023-08-30 22:09:34 +0300

    Bug fix in test

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,14 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 100; // на счете у клиента
-        int add = 10000; // сумма пополнения счета
-        int all = amount + add;
+        int add = 1050; // сумма пополнения счета
 
         System.out.println("Сумма денежных средств на Вашем счете составляет:");
         System.out.println((amount + add) + " рублей");
 
-        int bonus = all - 1000;
-        if (bonus > 1000) {
-            bonus = all - 1000;
+        int bonus;
+        if (add > 1000) {
+            bonus = add / 100;
         } else {
             bonus = 0;
         }
